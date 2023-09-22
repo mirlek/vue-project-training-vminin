9ef214c0b317b271b1a3d45de8a1d6e80e050e0e
mirlek <mirlenko.ekat@gmail.com>
2023-09-22 13:36:49 +0200

    Login inputs validation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,47 +1,109 @@
 <template>
- <div class="grey darken-1 empty-layout">
-
-
-
-<form class="card auth-card">
-  <div class="card-content">
-    <span class="card-title">Домашняя бухгалтерия</span>
-    <div class="input-field">
-      <input
+  <form class="card auth-card" @submit.prevent="submitHandler">
+    <div class="card-content">
+      <span class="card-title">Домашняя бухгалтерия</span>
+      <div class="input-field">
+        <input
           id="email"
           type="text"
-          class="validate"
-      >
-      <label for="email">Email</label>
-      <small class="helper-text invalid">Email</small>
-    </div>
-    <div class="input-field">
-      <input
+          v-model.trim="email"
+          :class="{
+            invalid:
+              (v$.email.$dirty && !v$.email.required) ||
+              (v$.email.$dirty && !v$.email.email),
+          }"
+        />
+        <label for="email">Email</label>
+        <small
+          class="helper-text invalid"
+          v-for="error of v$.email.$errors"
+          :key="error.$uid"
+        >
+          {{ error.$message }}
+        </small>
+      </div>
+
+      <div class="input-field">
+        <input
           id="password"
           type="password"
-          class="validate"
-      >
-      <label for="password">Пароль</label>
-      <small class="helper-text invalid">Password</small>
+          v-model.trim="password"
+          :class="{
+            invalid:
+              (v$.password.$dirty && !v$.password.required) ||
+              (v$.password.$dirty && !v$.password.minLength),
+          }"
+        />
+        <label for="password">Пароль</label>
+        <small
+          class="helper-text invalid"
+          v-for="error of v$.password.$errors"
+          :key="error.$uid"
+        >
+          {{ error.$message }}
+        </small>
+      </div>
     </div>
-  </div>
-  <div class="card-action">
-    <div>
-      <button
-          class="btn waves-effect waves-light auth-submit"
-          type="submit"
-      >
-        Войти
-        <i class="material-icons right">send</i>
-      </button>
+    <div class="card-action">
+      <div>
+        <button class="btn waves-effect waves-light auth-submit" type="submit">
+          Войти
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
+
+      <p class="center">
+        Нет аккаунта?
+        <router-link to="/register">Зарегистрироваться</router-link>
+      </p>
     </div>
-
-    <p class="center">
-      Нет аккаунта?
-      <a href="/">Зарегистрироваться</a>
-    </p>
-  </div>
-</form>
-
-</div>
+  </form>
 </template>
+
+<script>
+import { useVuelidate } from "@vuelidate/core";
+import { email, required, minLength, helpers } from "@vuelidate/validators";
+
+export default {
+  setup: () => {
+    return { v$: useVuelidate() };
+  },
+  data: () => ({
+    email: "",
+    password: "",
+  }),
+  validations: {
+    email: {
+      email: helpers.withMessage("Введите корректный Email", email),
+      required: helpers.withMessage(
+        "Поле Email не должно быть пустым",
+        required
+      ),
+    },
+    password: {
+      required: helpers.withMessage("Введите пароль", required),
+      minLength: helpers.withMessage(
+        ({ $params, $model }) =>
+          `Пароль должен быть ${$params.min} символов. Сейчас он  ${$model.length}`,
+        minLength(6)
+      ),
+    },
+  },
+  methods: {
+    submitHandler() {
+      console.log(this.v$);
+      if (this.v$.$invalid) {
+        this.v$.$touch();
+        return;
+      }
+      const formData = {
+        email: this.email,
+        password: this.password,
+      };
+
+      console.log(formData);
+      this.$router.push("/");
+    },
+  },
+};
+</script>
